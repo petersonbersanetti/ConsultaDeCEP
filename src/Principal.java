37f57a4ea20379fae06dd1a1b9635bafec5acf67
Peterson Bersanetti <peterson.bersanetti@gmail.com>
2023-06-06 23:06:24 -0300

    Consulta de CEP

---
+++ src/Principal.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        System.out.println("Digite um número de CEP para consulta: ");
+        var cep = sc.nextLine();
+
+        try{
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+    }
+
+}
+
